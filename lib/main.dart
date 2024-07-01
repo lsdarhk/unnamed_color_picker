90a77c4cd8e8b5822ae5dbdbed2d181a143a3f4a
Neal K <85104272+lsdarhk@users.noreply.github.com>
2024-07-01 17:34:13 +0700

    added provider. still isn't working as intended

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:unnamed_color_picker/pages/homepage.dart';
 
-void main() => runApp(const ColorPickerApp());
+void main() => runApp(
+      ChangeNotifierProvider(
+        create: (context) => ColorPickerAppState(),
+        child: const ColorPickerApp(),
+      ),
+    );
 
 class ColorPickerApp extends StatelessWidget {
   const ColorPickerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Homepage(),
+    final ColorPickerAppState appState =
+        Provider.of<ColorPickerAppState>(context);
+    return MaterialApp(
+      home: const Homepage(),
+      theme: ThemeData(
+        colorSchemeSeed: Colors.redAccent.shade200,
+        textTheme: GoogleFonts.changaTextTheme(),
+        brightness: Brightness.dark,
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: appState.themeMode,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,11 +1,88 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  Widget paletteContainers(int r, int g, int b) {
+    r = r.clamp(0, 255);
+    g = g.clamp(0, 255);
+    b = b.clamp(0, 255);
+    const dynamic size = 165;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(15),
+          height: size,
+          width: size,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Color.fromRGBO(r, g, b, 1.0)),
+        ),
+        Container(
+          margin: const EdgeInsets.all(15),
+          height: size,
+          width: size,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Color.fromRGBO(255 - r, 255 - g, 255 - b, 1.0)),
+        )
+      ],
+    );
+  }
+
+  final appState = ColorPickerAppState();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          paletteContainers(203, 75, 70),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Pick a color'),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () => appState.changeToLightTheme(),
+                child: const Text('Light mode'),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                onPressed: () => appState.changeToDarkTheme,
+                child: const Text('Dark mode'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
+class ColorPickerAppState extends ChangeNotifier {
+  ThemeMode themeMode = ThemeMode.system;
+  void changeToLightTheme() {
+    themeMode = ThemeMode.light;
+    notifyListeners();
+  }
+
+  void changeToDarkTheme() {
+    themeMode = ThemeMode.dark;
+    notifyListeners();
+  }
+}
